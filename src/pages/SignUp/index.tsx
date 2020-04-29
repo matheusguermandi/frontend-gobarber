1c000d1fb832f4d1034519b353839b952d0796d4
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-04-29 16:58:56 -0300

    Using @unform/core and @unform/web

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
@@ -8,36 +8,46 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+  /*
+    Inicializar input com defaultValue
+    <Form initialData={{ name: 'Matheus' }} onSubmit={handleSubmit}>
+  */
+  return (
+    <Container>
+      <Background />
 
-      <form>
-        <h1>Faça seu Cadastro</h1>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
 
-        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-      <a href="login">
-        <FiArrowLeft />
-        Já sou cadastrado
-      </a>
-    </Content>
-  </Container>
-);
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Já sou cadastrado
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
