b59999510078d87ac35bfef8716712cefddaa3ce
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-19 19:03:04 -0300

    Adicionando página de reset password no index de rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,8 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
@@ -14,6 +16,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
     <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
