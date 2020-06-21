57da1d0f94e70ed0b4bda209e7467cf73e57c673
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-06-20 23:16:18 -0300

    Adicionando função de SignOut

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,26 +5,34 @@ import { FiPower } from 'react-icons/fi';
 import { Container, Header, HeaderContent, Profile } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
-const Dashboard: React.FC = () => (
-  <Container>
-    <Header>
-      <HeaderContent>
-        <img src="{logo}" alt="GoBarber" />
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
 
-        <Profile>
-          <img src="" alt="" />
-          <div>
-            <span>Bem-vindo</span>
-            <strong>...</strong>
-          </div>
-        </Profile>
-        <button type="button">
-          <FiPower />
-        </button>
-      </HeaderContent>
-    </Header>
-  </Container>
-);
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://avatars2.githubusercontent.com/u/27836893?s=460&u=4d9a53c02a3d30c2e61c992f9ba603547129bfe0&v=4"
+              alt="Matheus Guermandi"
+            />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>Matheus Guermandi</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
